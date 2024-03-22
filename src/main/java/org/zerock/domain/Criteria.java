ff25583d332fa1bbae4b43504d7c08fe989b1ce3
beomgeun <jhchoi524@gmail.com>
2024-03-22 12:20:42 +0900

    03-22 마스터v2 준비

--- src/main/java/org/zerock/domain/Criteria.java
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -1,7 +1,5 @@
 package org.zerock.domain;
 
-import org.springframework.web.util.UriComponentsBuilder;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,9 +13,6 @@ public class Criteria {
   private int pageNum;
   private int amount;
   
-  private String type;
-  private String keyword;
-
   public Criteria() {
     this(1, 10);
   }
@@ -27,20 +22,5 @@ public class Criteria {
     this.amount = amount;
   }
   
-  public String[] getTypeArr() {
-    
-    return type == null? new String[] {}: type.split("");
-  }
-  
-  public String getListLink() {
-	 
-	  UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
-				.queryParam("pageNum", this.pageNum)
-				.queryParam("amount", this.getAmount())
-				.queryParam("type", this.getType())
-				.queryParam("keyword", this.getKeyword());
-
-		return builder.toUriString();
-
-  }
+ 
 }
